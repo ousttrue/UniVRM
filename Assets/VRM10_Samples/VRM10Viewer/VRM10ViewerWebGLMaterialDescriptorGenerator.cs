ffea1dc5ccaf3182d3c3817d4b0a1848cdd9ebcb
ousttrue <ousttrue@gmail.com>
2025-01-30 15:37:24 +0900

    VRM10ViewerWebGLMaterialDescriptorGenerator

--- Assets/VRM10_Samples/VRM10Viewer/VRM10ViewerWebGLMaterialDescriptorGenerator.cs
+++ Assets/VRM10_Samples/VRM10Viewer/VRM10ViewerWebGLMaterialDescriptorGenerator.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UniGLTF;
 using UnityEngine;
 
@@ -5,6 +7,9 @@ namespace UniVRM10.VRM10Viewer
 {
     public class VRM10ViewerWebGLMaterialDescriptorGenerator : IMaterialDescriptorGenerator
     {
+        private readonly Material _Lit;
+        private readonly Material _MToon;
+
         /// <summary>
         /// PBRとMToonのマテリアルをカスタマイズする。
         /// unlit は既存のものを使う。
@@ -13,17 +18,41 @@ namespace UniVRM10.VRM10Viewer
         /// <param name="MToon"></param>
         public VRM10ViewerWebGLMaterialDescriptorGenerator(Material Lit, Material MToon)
         {
+            if (Lit == null) throw new ArgumentNullException("Lit");
+            _Lit = Lit;
 
+            if (MToon == null) throw new ArgumentNullException("MToon");
+            _MToon = MToon;
         }
 
         public MaterialDescriptor Get(GltfData data, int i)
         {
-            throw new System.NotImplementedException();
+            var matDesc = new MaterialDescriptor(
+                $"Material#{i}",
+                _Lit.shader,
+                null,
+                new Dictionary<string, TextureDescriptor>(),
+                new Dictionary<string, float>(),
+                new Dictionary<string, Color>(),
+                new Dictionary<string, Vector4>(),
+                new List<Action<Material>>()
+            );
+            return matDesc;
         }
 
         public MaterialDescriptor GetGltfDefault(string materialName = null)
         {
-            throw new System.NotImplementedException();
+            // FIXME
+            return new MaterialDescriptor(
+                string.IsNullOrEmpty(materialName) ? "__default__" : materialName,
+                _Lit.shader,
+                default,
+                new Dictionary<string, TextureDescriptor>(),
+                new Dictionary<string, float>(),
+                new Dictionary<string, Color>(),
+                new Dictionary<string, Vector4>(),
+                new List<Action<Material>>()
+            );
         }
     }
 }
